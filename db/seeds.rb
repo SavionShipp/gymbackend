94316e40b866533727c381361f7700a1ecefeee8
SavionShipp <shippsavion209@yahoo.com>
2024-11-12 16:36:57 -0800

    added jbuilder and controller

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,5 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+Exercise.create(name: "Jim", description: "his name is Jim", image_url: "", video_url: "")
